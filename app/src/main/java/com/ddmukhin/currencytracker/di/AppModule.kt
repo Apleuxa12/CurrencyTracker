2af25b8c578a23fa146eee3a80c0e26384c68836
ddmukhin <dmitrij.muhin@stroyland.ru>
2021-12-20 20:36:06 +0300

    dependencies fix

--- app/src/main/java/com/ddmukhin/currencytracker/di/AppModule.kt
+++ app/src/main/java/com/ddmukhin/currencytracker/di/AppModule.kt
@@ -8,19 +8,23 @@ import com.ddmukhin.currencytracker.data.persistence.CurrencyDao
 import com.ddmukhin.currencytracker.data.persistence.PersistenceRepository
 import com.ddmukhin.currencytracker.data.persistence.PersistenceRepositoryImpl
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class AppModule {
 
+    @Provides
     fun provideCurrencyRepository(
         currencyService: CurrencyService,
         currenciesConverter: CurrenciesConverter
     ): CurrencyRepository = CurrencyRepositoryImpl(currencyService, currenciesConverter)
 
+    @Provides
     fun providePersistenceRepository(
         currencyDao: CurrencyDao,
         currenciesConverter: CurrenciesConverter
--- app/src/main/java/com/ddmukhin/currencytracker/di/PersistenceModule.kt
+++ app/src/main/java/com/ddmukhin/currencytracker/di/PersistenceModule.kt
@@ -5,9 +5,11 @@ import androidx.room.Room
 import com.ddmukhin.currencytracker.data.persistence.AppDataBase
 import com.ddmukhin.currencytracker.data.persistence.CurrencyDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,6 +19,8 @@ class PersistenceModule {
         const val DB_NAME = "currency_tracker_db"
     }
 
+    @Provides
+    @Singleton
     fun provideDatabase(@ApplicationContext applicationContext: Context): AppDataBase =
         Room.databaseBuilder(
             applicationContext,
@@ -24,5 +28,6 @@ class PersistenceModule {
             DB_NAME
         ).build()
 
+    @Provides
     fun provideCurrencyDao(database: AppDataBase): CurrencyDao = database.currencyDao()
 }
